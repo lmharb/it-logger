3caa17a07ef3454c5d52c338f9734226522ecf44
lmharb <lm.harb59@gmail.com>
2021-12-21 17:06:42 +0100

    Added deleteTech action

--- src/reducers/techReducer.js
+++ src/reducers/techReducer.js
@@ -23,6 +23,14 @@ export default (state = initialState, action) => {
         techs: [...state.techs, action.payload],
         loading: false,
       }
+    case DELETE_TECH:
+      return {
+        ...state,
+        techs: state.techs.filter((tech) => tech.id !== action.payload),
+        loading: false,
+      }
+    case SET_LOADING:
+      return { ...state, loading: true }
     case TECHS_ERROR:
       console.error(action.payload)
       return {
@@ -30,8 +38,6 @@ export default (state = initialState, action) => {
         error: action.payload,
         loading: false,
       }
-    case SET_LOADING:
-      return { ...state, loading: true }
     default:
       return state
   }
