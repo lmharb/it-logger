62d8fef0951894f887b0baee18226172e78d8854
lmharb <lm.harb59@gmail.com>
2021-12-21 16:51:32 +0100

    Added addTech action & updated AddTechModal

--- src/reducers/techReducer.js
+++ src/reducers/techReducer.js
@@ -17,6 +17,19 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case GET_TECHS:
       return { ...state, techs: action.payload, loading: false }
+    case ADD_TECH:
+      return {
+        ...state,
+        techs: [...state.techs, action.payload],
+        loading: false,
+      }
+    case TECHS_ERROR:
+      console.error(action.payload)
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      }
     case SET_LOADING:
       return { ...state, loading: true }
     default:
